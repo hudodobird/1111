95041dca6978f7bf4b020c6bdb9b6562351bb277
hudodobird <159005081+hudodobird@users.noreply.github.com>
2025-12-08 15:31:01 +0000

    Idk part something

--- 1111.py
+++ 1111.py
@@ -1,40 +1,51 @@
-import random
+# import random
 
-letters = "abcdefjhijklmnopqrstuvwxyz"
-randomletters = ""
-
-
-def generateletters(randomletters):
-    randomint = random.randint(1, 10)
-    for i in range(randomint):
-        randomnum = random.randint(0, len(letters) - 1)
-        randomletters = randomletters + letters[randomnum]
-    return randomletters
-
-def validateAnswer(randomletters, answer):
-
-    for letter in answer:
-        if letter not in randomletters:
-            return "Invalid answer"
-        else:
-            pass
-
-    return len(answer)
+# letters = "abcdefjhijklmnopqrstuvwxyz"
+# randomletters = ""
 
 
+# def generateletters(randomletters):
+#     randomint = random.randint(1, 10)
+#     for i in range(randomint):
+#         randomnum = random.randint(0, len(letters) - 1)
+#         randomletters = randomletters + letters[randomnum]
+#     return randomletters
 
-# def validateAnswer(randomLetters, answer):
-#     for letters in answer:
-#         if letters not in randomLetters:
-#             return 0
+# def validateAnswer(randomletters, answer):
+
+#     for letter in answer:
+#         if letter not in randomletters:
+#             return "Invalid answer"
 #         else:
 #             pass
-    
+
 #     return len(answer)
 
-generated = generateletters(randomletters)
-print(generated)
 
-answer = input("Enter your answer: ")
 
-print(validateAnswer(generated, answer))
+# # def validateAnswer(randomLetters, answer):
+# #     for letters in answer:
+# #         if letters not in randomLetters:
+# #             return 0
+# #         else:
+# #             pass
+    
+# #     return len(answer)
+
+# generated = generateletters(randomletters)
+# print(generated)
+
+# answer = input("Enter your answer: ")
+
+# print(validateAnswer(generated, answer))
+
+
+
+
+evens = [i for i in range(0, 10, 2)]
+# evens = []
+
+# for i in range(0, 10, 2):
+#     evens.append(i)
+
+print(evens)
